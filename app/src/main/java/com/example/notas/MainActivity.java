e468800664c3559c1c889d7004ad9f613104dc70
~mikhers <~miguel.morenovuniagustiniana.edu.co>
2023-03-02 17:46:17 -0500

    Se agregó el permiso de GPS

--- app/src/main/java/com/example/notas/MainActivity.java
+++ app/src/main/java/com/example/notas/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.notas;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,15 +24,19 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    //EditText
+    //Para usar el GPS creo que hay que usar LocationRequest
     GoogleMap mMap;
+    int REQUEST_CODE = 200;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        verificarGPS();
     }
 
     @Override
@@ -33,5 +45,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         LatLng colombia = new LatLng(4.653130, -74.144602);
         mMap.addMarker(new MarkerOptions().position(colombia).title("Uniagustiniana"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(colombia));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void verificarGPS() {
+        int permisionGPS = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        if (permisionGPS==PackageManager.PERMISSION_GRANTED)
+            Toast.makeText(this, "Permiso de ubicación concedido",Toast.LENGTH_SHORT).show();
+        else requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE);
+
     }
 }
